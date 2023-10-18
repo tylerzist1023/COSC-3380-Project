0130a1d101257bf1658fb9a8b4f27654e2249aea
Tyler Zistler <89903973+tylerzist1023@users.noreply.github.com>
2023-10-18 18:54:31 -0500

    SQL server test

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import pyodbc
+
+conn = pyodbc.connect('DRIVER={SQL Server};'
+                      'SERVER=coogmusicserver.windows.database.net;'
+                      'DATABASE=coogmusic;'
+                      'UID=CloudSA5fb1e56b;'
+                      'PWD=Coogs4lyfe!;')
+cursor = conn.cursor()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='John Doe')
+    cursor.execute('SELECT * FROM song')
+    data = cursor.fetchall()
+    return render_template('index.html', name=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
