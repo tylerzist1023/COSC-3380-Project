2dd0be2408fde9177c3ce98b1c2a74db2d5d2b0f
spartan24032 <118637167+spartan24032@users.noreply.github.com>
2023-11-21 06:30:50 -0600

    new file:   nodejs/public/style_admin.css 	modified:   nodejs/server.js 	modified:   nodejs/templates/admin.html 	new file:   nodejs/templates/insights.html

--- nodejs/server.js
+++ nodejs/server.js
@@ -12,6 +12,7 @@ import  Types  from 'formidable';
 import { Readable } from 'stream';
 import {fileTypeFromBuffer} from 'file-type';
 import { getAudioDurationInSeconds } from 'get-audio-duration';
+import { match } from 'assert';
 
 // import nodemon from 'nodemon';
 
@@ -50,7 +51,13 @@ function captureUrl(userUrl, url) {
     let match = userUrl.match(new RegExp(`^${url}(\\?.*)?\\/?$`, 'g')); 
     return match;
 }
+function joshieboymatch(InBound,match){
+    if(InBound.replace(match,"").length!=InBound.length){
+        return true
+    }
+return false
 
+}
 function matchUrl(userUrl, url) {
     return captureUrl(userUrl, url) !== null;
 }
@@ -117,6 +124,7 @@ function serveStatic_Plus(res, filePath, contentType, replacements, responseCode
                 });
             }
             let conditionalContent = '';
+            if('role' in replacements){
             if (replacements['role'] === 'listener') {
                 conditionalContent = `
                     <div class="input_box">
@@ -139,9 +147,11 @@ function serveStatic_Plus(res, filePath, contentType, replacements, responseCode
             data = data.replace('{{conditionalContent}}', conditionalContent);
 
 
-            res.writeHead(responseCode, { 'Content-Type': contentType });
-            res.end(data);
+            
         }
+        res.writeHead(responseCode, { 'Content-Type': contentType });
+            res.end(data);
+    }
     });
 }
 function serveStaticFile(res, filePath, contentType, responseCode = 200) {
@@ -158,30 +168,39 @@ function serveStaticFile(res, filePath, contentType, responseCode = 200) {
 
 
 
-const getAdminBaseData= async (userId) => {
+const getAdminBaseData= async () => {
     try {
         const data = {};
+        const NumArtist = await executeQuery('SELECT COUNT(*) FROM Artist');
+        const NumUsers =await executeQuery('SELECT COUNT(*) FROM Listener');
+        const NumSongs = await executeQuery('SELECT COUNT(*) FROM Song');
+        const NumPlaylist = await executeQuery('SELECT COUNT(*) FROM Playlist')
 
+
+        data['NumArtist']=NumArtist
+        data['NumUsers'] = NumUsers
+        data['NumSongs'] = NumSongs
+        data['NumPlaylist'] = NumPlaylist
         // Get all the new songs added
-        const NewSongQuery = 'SELECT SongID,Song.Name AS SongName, Artist.ArtistName, Artist.ProfilePic FROM Song JOIN Artist ON Song.ArtistID = Artist.ArtistID ORDER BY Song.CreationTimestamp DESC LIMIT 5';
-        const NewSongResults = await executeQuery(NewSongQuery);
-        data['NewSongs'] = NewSongResults;
+        // const NewSongQuery = 'SELECT SongID,Song.Name AS SongName, Artist.ArtistName as ArtistName, Artist.ProfilePic as ProfilePic FROM Song JOIN Artist ON Song.ArtistID = Artist.ArtistID ORDER BY Song.CreationTimestamp';
+        // const NewSongResults = await executeQuery(NewSongQuery);
+        // data['NewSongs'] = NewSongResults;
 
 
         // Get All the new Artist 
-        const NewArtistQuery = 'SELECT ArtistName, ProfilePic,ArtistID FROM Artist ORDER BY CreationStamp DESC LIMIT 5';
-        const NewArtistResults = await executeQuery(NewArtistQuery);
+        // const NewArtistQuery = 'SELECT ArtistName, ProfilePic,ArtistID FROM Artist ORDER BY CreationStamp DESC';
+        // const NewArtistResults = await executeQuery(NewArtistQuery);
 
         //get all the pictures 
 
-        NewArtistResults.forEach(artist => {
-            if (artist.ProfilePic && Buffer.isBuffer(artist.ProfilePic)) {
-                // Convert the Buffer to a Base64 string
-                artist.ProfilePic = artist.ProfilePic.toString('base64');
-            }
-        });
+        // NewArtistResults.forEach(artist => {
+        //     if (artist.ProfilePic && Buffer.isBuffer(artist.ProfilePic)) {
+        //         // Convert the Buffer to a Base64 string
+        //         artist.ProfilePic = artist.ProfilePic.toString('base64');
+        //     }
+        // });
 
-        data['NewArtist'] = NewArtistResults;
+        // data['NewArtist'] = NewArtistResults;
         return data;
     } catch (err) {
         throw new Error(`Error in getListenerBaseData: ${err.message}`);
@@ -216,6 +235,33 @@ async function getAdmin(sessionData) {
         //got the data base
         const data = await getAdminBaseData();
         return data
+    } catch (error) {
+        console.error(error);
+        return {error: "Error"};
+    }
+}
+//get all the insight data
+async function getAdminAll() {
+    try {
+        const data = {};
+
+        const NewArtistQuery = 'SELECT ArtistName, ProfilePic,ArtistID FROM Artist ORDER BY CreationStamp DESC';
+        const NewArtistResults = await executeQuery(NewArtistQuery);
+
+
+
+        NewArtistResults.forEach(artist => {
+            if (artist.ProfilePic && Buffer.isBuffer(artist.ProfilePic)) {
+                // Convert the Buffer to a Base64 string
+                artist.ProfilePic = artist.ProfilePic.toString('base64');
+            }
+        });
+
+        data['NewArtist'] = NewArtistResults;
+        return data;
+
+
+
     } catch (error) {
         console.error(error);
         return {error: "Error"};
@@ -420,10 +466,33 @@ const server = http.createServer(async (req, res) => {
         res.writeHead(404);
         res.end('Not Found');
     }
+    else if(joshieboymatch(req.url,'/admin/insights/')){
+
+
+        const profile = (req.url.replace('/admin/insights/',""));
+        if(profile==='type'){
+            res.end(JSON.stringify(await getAdminAll(sessionData, res)));
+        }
+        else{
+            serveStatic_Plus(res,"./templates/insights.html","", {'profile':profile})
+
+        }
+    
+
+
+
+        
+
+    }
     // css of the homepage
     else if (req.url === '/styles.css') {
         serveStaticFile(res, './public/styles.css', 'text/css');
     }
+    else if (req.url === '/style_admin.css'){
+        serveStaticFile(res, './public/style_admin.css', 'text/css');
+    } 
+
+    //if you need a picture of an album that does not exist
     else if (req.url === '/npc/picture' ) {
       serveStaticFile(res, './public/cartoon-mysterious.png', 'image/png')
     } 
@@ -447,7 +516,11 @@ const server = http.createServer(async (req, res) => {
     else if(req.url === "/getstarted"){
         serveStaticFile(res, './templates/login_options.html', 'text/html');
     }
+
     else if(req.url === "/topbar"){
+
+
+  
         if(getRole(sessionData) === 'listener') {
             let html = `<div class="topbar">
             <ul class="topbar_navigation">
