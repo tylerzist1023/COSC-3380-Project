f4f1f6c2f06b93c394eabc0fa46195b1e8849c09
tylerzist1023 <89903973+tylerzist1023@users.noreply.github.com>
2023-11-23 09:21:08 -0600

    Player automatically goes to the next song

--- nodejs/public/base.js
+++ nodejs/public/base.js
@@ -13,3 +13,51 @@ function getSidebar(x) {
         document.getElementById('leftBar').innerHTML = data;
     });
 }
+
+function playSong(this_) {
+    if(this_ === null || this_ === undefined) {
+        return function() { e.preventDefault();};
+    }
+    // this may look a little weird, but if you want to pass in an event where the data varies, or if you want to pass in an event that proxies another object, you can create a function that returns a function and use the parameters of the top level function within the returned function. the "this_" object is the anchor object that the user is supposed to click on to play the desired song they want.
+    return function(e) {
+        e.preventDefault();
+
+        let songid = this_.getAttribute('songid');
+        // it's called "ii" because "i" is used as a variable for a for loop
+        let ii = this_.getAttribute('ii');
+        
+        fetch('/song/'+songid)
+            .then(response => response.json())
+            .then(data => {
+                let playerImage = document.getElementById('playerImage');
+                let playerSong = document.getElementById('playerSong');
+                let playerArtist = document.getElementById('playerArtist');
+                let playerSource = document.getElementById('playerSource');
+                let playerList = document.getElementById(`list${songid}`);
+
+                // one problem: the "hover" stops working. i don't know how to fix it!
+                const trackLists = document.querySelectorAll('.track_list');
+                for (let i = 0; i < trackLists.length; i++) {
+                    trackLists[i].style.backgroundColor = '#111727';
+                }
+                playerList.style.backgroundColor = '#636363';
+                playerImage.setAttribute('src', '/album/'+data['albumid']+'/pic');
+                playerSong.innerHTML = data['songname'];
+                playerArtist.innerHTML = data['artistname'];            
+            })
+            .finally(() => {
+                let playerSource = document.getElementById('playerSource');
+                let playerAudio = document.getElementById('playerAudio');
+                playerSource.setAttribute('src', '/song/'+songid+'/audio');
+                playerAudio.load();
+                playerAudio.play();
+                let playerDiv = document.getElementById('playerDiv');
+                playerDiv.style.display = '';
+
+                let iiplus1 = parseInt(ii)+1;
+                let nextSongLink = document.querySelector('[ii="'+iiplus1+'"]');
+                playerAudio.onended = playSong(nextSongLink);
+            })
+            .catch(error => console.error('Error:', error));
+    }
+}
